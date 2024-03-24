1a836c876812810402fb6ddd9a6bf8439e11b534
HACK-WITH-JS <jungmini2040@gmail.com>
2024-03-24 15:22:16 +0900

    PATH 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { PATH } from "./constants/path";
+import Navbar from "./components/shared/Navbar";
+
+const { HOME, MY } = PATH;
+
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<div>HomePage</div>} />
-        <Route path="/my" element={<div>MyPage</div>} />
+        <Route path={HOME} element={<div>HomePage</div>} />
+        <Route path={MY} element={<div>MyPage</div>} />
       </Routes>
     </BrowserRouter>
   );
